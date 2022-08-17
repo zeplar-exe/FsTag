a3526dec313672c5d08f32189cf471511736c469
Zander Baker <zanderbaker100@gmail.com>
2022-08-17 14:35:37 -0700

    Add tutorial command to generate playground directory

---
+++ FsTag/Commands/TutorialCommand.cs
@@ -0,0 +1,91 @@
+﻿using CommandDotNet;
+
+using FsTag.Extensions;
+
+namespace FsTag;
+
+public partial class Program
+{
+    private const string TestDirectoryName = "fstag_test";
+    
+    [Command("tutorial")]
+    [Subcommand]
+    public class TutorialCommand
+    {
+        [DefaultCommand]
+        public int Interactive()
+        {
+            Console.WriteLine("Nothing yet.");
+            
+            return 0;
+        }
+
+        [Command("init")]
+        public int Init([Option('f', "force")] bool force = false)
+        {
+            var root = Path.Join(Directory.GetCurrentDirectory(), TestDirectoryName);
+
+            if (Directory.Exists(root))
+            {
+                if (!force)
+                {
+                    WriteFormatter.Error($"The {TestDirectoryName} directory already exists in the current directory. " +
+                                         "Did you mean to use -f?");
+                    
+                    return 1;
+                }
+                
+                Directory.Delete(root, true);
+            }
+
+            var directory = Directory.CreateDirectory(root);
+
+            directory.CreateFile("test.txt");
+            directory.CreateFile("test1.txt");
+            directory.CreateFile("test2.txt");
+            directory.CreateFile("test3.txt");
+
+            var aDir = directory.CreateSubdirectory("A");
+
+            aDir.CreateFile("A_file_of_the_will");
+            aDir.CreateFile("A_pizza");
+            aDir.CreateFile("A.pdf");
+            aDir.CreateFile("Apothecary.txt");
+
+            var underDir = directory.CreateSubdirectory("_under");
+
+            underDir.CreateFile("_abc_");
+            underDir.CreateFile("_abc_123_.txt");
+            underDir.CreateFile("abc_123.jpg");
+            
+            var realUnderDir = directory.CreateSubdirectory("realunder");
+            
+            realUnderDir.CreateFile("_abc_");
+            realUnderDir.CreateFile("_abc_123_.txt");
+            realUnderDir.CreateFile("abc_123.jpg");
+            
+            Console.WriteLine($"Created {TestDirectoryName} in the current directory.");
+
+            return 0;
+        }
+
+        [Command("deinit")]
+        public int Deinit()
+        {
+            var path = Path.Join(Directory.GetCurrentDirectory(), TestDirectoryName);
+
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+                
+                Console.WriteLine($"Deleted {TestDirectoryName} in the current directory.");
+            }
+            else
+            {
+                WriteFormatter.Info($"The {TestDirectoryName} directory does not exist.");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ FsTag/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FsTag.Extensions;
+
+internal static class DirectoryInfoExtensions
+{
+    public static FileInfo CreateFile(this DirectoryInfo directory, string relativePath)
+    {
+        var info = new FileInfo(Path.Join(directory.FullName, relativePath));
+
+        info.Create().Dispose();
+        
+        return info;
+    }
+}
