3ca9f2ba6a66344018e0cdab7547c407b13ff172
Zander Baker <zanderbaker100@gmail.com>
2022-08-19 20:37:23 -0700

    Extract regex format pairs
    [Class]: Create RegexFormatPair, FormatPairBuilder in Common/
    [Class]: Adapt PrintCommand to extraction

--- FsTag/Commands/PrintCommand.cs
+++ FsTag/Commands/PrintCommand.cs
@@ -2,6 +2,8 @@
 
 using CommandDotNet;
 
+using FsTag.Common;
+
 namespace FsTag;
 
 public partial class Program
@@ -51,48 +53,5 @@ public partial class Program
 
             return 0;
         }
-
-        private class FormatPairBuilder
-        {
-            private List<FormatPair> Pairs { get; }
-
-            public FormatPairBuilder()
-            {
-                Pairs = new List<FormatPair>();
-            }
-
-            public SinglePair HandleFormat(Regex regex)
-            {
-                return new SinglePair(regex, this);
-            }
-
-            public List<FormatPair> Build()
-            {
-                return Pairs;
-            }
-
-            public class SinglePair
-            {
-                private Regex Regex { get; }
-                private FormatPairBuilder Builder { get; }
-
-                public SinglePair(Regex regex, FormatPairBuilder builder)
-                {
-                    Regex = regex;
-                    Builder = builder;
-                }
-
-                public FormatPairBuilder With(Func<Match, int> handler)
-                {
-                    var pair = new FormatPair(Regex, handler);
-                    
-                    Builder.Pairs.Add(pair);
-
-                    return Builder;
-                }
-            }
-        }
-
-        private record FormatPair(Regex Regex, Func<Match, int> Handle);
     }
 }
---
+++ FsTag/Common/RegexFormatPair.cs
@@ -0,0 +1,5 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FsTag.Common;
+
+public record RegexFormatPair(Regex Regex, Func<Match, int> Handle);
