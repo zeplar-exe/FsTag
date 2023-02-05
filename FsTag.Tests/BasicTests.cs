326d7f36344c19e192eb5b414ac44775d91d3ab6
Zander Baker <zanderbaker100@gmail.com>
2023-02-05 14:29:42 -0800

    remove redundant test

--- FsTag.Tests/BasicTests.cs
+++ FsTag.Tests/BasicTests.cs
@@ -3,12 +3,6 @@ namespace FsTag.Tests;
 public class BasicTests : TestBase
 {
     [Test]
-    public void TestBase()
-    {
-        Executable.AssertExitCode(1); // No params is an "error": Code 1
-    }
-    
-    [Test]
     public void TestPrintExit()
     {
         Executable.AssertExitCode(0, "print");
