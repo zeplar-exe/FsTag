04fb1597935bd687e51611faf2c8dc56f4065e74
Zander Baker <zanderbaker100@gmail.com>
2023-02-16 18:30:28 -0800

    Fix docs command

--- FsTag/Commands/HelpCommand.cs
+++ FsTag/Commands/HelpCommand.cs
@@ -6,44 +6,64 @@ namespace FsTag;
 
 public partial class Program
 {
-    [LocalizedCommand("help", nameof(Descriptions.HelpCommand))]
+    [LocalizedCommand("docs", nameof(Descriptions.HelpCommand))]
     [Subcommand]
-    public class HelpCommand
+    public class DocsCommand
     {
         [DefaultCommand]
-        public int Execute()
+        public int Execute(string? module = null)
         {
-            WriteFormatter.Plain("Loaded Help Modules:");
+            var modules = GetDocumentationModules().ToArray();
 
-            foreach (var module in GetHelpModules())
+            if (module != null)
             {
-                WriteFormatter.PlainNoLine(module.Name);
-                WriteFormatter.PlainNoLine($"[ {module.FileName} ]");
+                foreach (var helpModule in modules)
+                {
+                    if (helpModule.IsMatch(module))
+                    {
+                        WriteFormatter.Plain(helpModule.Content);
+
+                        return 0;
+                    }
+                }
+
+                return 1;
+            }
+            
+            WriteFormatter.Plain("Loaded Documentation Modules:");
+
+            foreach (var helpModule in modules)
+            {
+                WriteFormatter.PlainNoLine(helpModule.Name);
+                WriteFormatter.PlainNoLine($" [ {helpModule.FileName} ]");
                 
                 WriteFormatter.NewLine();
             }
 
             return 0;
         }
-        
-        [Command("filters")]
-        public int Filters()
-        {
 
-
-            return 0;
-        }
-
-        private IEnumerable<HelpModule> GetHelpModules()
+        private IEnumerable<DocumentationModule> GetDocumentationModules()
         {
             var directory = Path.Join(Directory.GetCurrentDirectory(), "docs");
 
             foreach (var file in Directory.EnumerateFiles(directory, "*.md"))
             {
-                yield return new HelpModule(Path.GetFileNameWithoutExtension(file),  Path.GetFileName(file));
+                yield return new DocumentationModule(
+                    Path.GetFileNameWithoutExtension(file),  
+                    Path.GetFileName(file), 
+                    file);
             }
         }
 
-        private record HelpModule(string Name, string FileName);
+        private record DocumentationModule(string Name, string FileName, string FilePath)
+        {
+            public string Content => File.ReadAllText(FilePath);
+            
+            public bool IsMatch(string name)
+            {
+                return name == Name || name == FileName;
+            }
+        }
     }
 }
